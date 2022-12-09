5497b72094b42b2a1a29866ece85e5f48f5f054c
energistix <50068022+energistix@users.noreply.github.com>
2022-12-09 19:30:03 +0100

    test

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,25 +1,27 @@
-import express from 'express'
-import path from 'path'
-import http from 'http'
+import express from "express"
+import path from "path"
+import http from "http"
 
 const port: number = 3000
 
 class App {
-    private server: http.Server
-    private port: number
+  private server: http.Server
+  private port: number
 
-    constructor(port: number) {
-        this.port = port
-        const app = express()
-        app.use(express.static(path.join(__dirname, '../client')))
-        this.server = new http.Server(app)
-    }
+  constructor(port: number) {
+    this.port = port
+    const app = express()
+    app.use(express.static(path.join(__dirname, "../client")))
+    this.server = new http.Server(app)
+  }
 
-    public Start() {
-        this.server.listen(this.port, () => {
-            console.log(`Server listening on port ${this.port}.`)
-        })
-    }
+  public Start() {
+    this.server.listen(this.port, () => {
+      console.log(`Server listening on port ${this.port}.`)
+    })
+  }
 }
 
 new App(port).Start()
+
+console.log("test")
